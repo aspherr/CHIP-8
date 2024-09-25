317c9874eb5e8305b6e623693d5c934b69b6031f
aspherr <huzaifa.syedd@gmail.com>
2024-09-25 21:31:24 +0100

    project config

---
+++ frontend/target/debug/deps/frontend-b6b8fe062975ab30.d
@@ -0,0 +1,5 @@
+/Users/huzaifa/Developer/Rust/CHIP-8/frontend/target/debug/deps/frontend-b6b8fe062975ab30: src/main.rs
+
+/Users/huzaifa/Developer/Rust/CHIP-8/frontend/target/debug/deps/frontend-b6b8fe062975ab30.d: src/main.rs
+
+src/main.rs:
---
+++ frontend/target/debug/frontend.d
@@ -0,0 +1,1 @@
+/Users/huzaifa/Developer/Rust/CHIP-8/frontend/target/debug/frontend: /Users/huzaifa/Developer/Rust/CHIP-8/frontend/src/main.rs
